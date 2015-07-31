bedf22b5e01f91bce0c86b4481a56a049948fb6a
Nicolas Briemant <nicolas.briemant@gmail.com>
2015-07-31 15:29:53 +0000

    add UI tools (material design & purecss.io)

--- app/app.js
+++ app/app.js
@@ -1,9 +1,27 @@
-import React, {Component} from 'react';
+import React from 'react';
+import mui from 'material-ui';
 
-class App extends Component {
+let ThemeManager = new mui.Styles.ThemeManager();
+
+class App extends React.Component {  
+  getChildContext() {
+    return {muiTheme: ThemeManager.getCurrentTheme()};
+  }
+
   render() {
-    return <span>Hello world.</span>;
+    return (
+      <div>
+        <mui.AppBar title="Wall Street" showMenuIconButton={false} />
+        <div className="pure-g">
+          <div className="pure-u-1-3"><p>Thirds</p></div>
+          <div className="pure-u-1-3"><p>Thirds</p></div>
+          <div className="pure-u-1-3"><p>Thirds</p></div>
+        </div>
+      </div>
+    );
   }
 }
+
+App.childContextTypes = {muiTheme: React.PropTypes.object};
 
 export default App;
