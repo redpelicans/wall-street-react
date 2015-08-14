cfa9d13449d823db7b512426d015014d3aa7e608
Nicolas Briemant <nicolas.briemant@gmail.com>
2015-08-14 19:15:10 +0000

    refine navigation

--- app/app.jsx
+++ app/app.jsx
@@ -1,26 +1,86 @@
+import _ from 'lodash';
 import React from 'react';
 import Router from 'react-router';
+import routes, {appRoutesData as menuItems} from './routes';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 import mui from 'material-ui';
 let ThemeManager = new mui.Styles.ThemeManager();
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.toggleLeftNav = this.toggleLeftNav.bind(this);
+    this.onLeftNavChange = this.onLeftNavChange.bind(this);
+    this.onLeftNavHeaderClick = this.onLeftNavHeaderClick.bind(this);
+    this.getSelectedIndex = this.getSelectedIndex.bind(this);
+  }
+
   getChildContext() {
     return {muiTheme: ThemeManager.getCurrentTheme()};
   }
 
+  componentWillMount() {
+    ThemeManager.setPalette({
+      primary1Color: mui.Styles.Colors.green500,
+      primary2Color: mui.Styles.Colors.green700,
+      primary3Color: mui.Styles.Colors.green100,
+    });
+  }
+
+  toggleLeftNav() {
+    this.refs.leftNav.toggle();
+  }
+
+  getSelectedIndex() {
+    return _.findIndex(menuItems, (menuItem) => {
+      return menuItem.route && this.context.router.isActive(menuItem.route);
+    });
+  }
+
+  onLeftNavChange(event, key, payload) {
+    this.context.router.transitionTo(payload.route);
+  }
+
+  onLeftNavHeaderClick() {
+    this.context.router.transitionTo('/');
+    this.refs.leftNav.close();
+  }
+
   render() {
+    let headerLeftNavStyle = {
+      cursor: 'pointer',
+      color: ThemeManager.getCurrentTheme().component.appBar.textColor,
+      backgroundColor: ThemeManager.getCurrentTheme().palette.primary1Color,
+      lineHeight: mui.Styles.Spacing.desktopKeylineIncrement + 'px',
+      paddingLeft: mui.Styles.Spacing.desktopGutter,
+    };
+
+    let header = (
+      <div style={headerLeftNavStyle} onTouchTap={this.onLeftNavHeaderClick}>Wall Street</div>
+    );
+
     return (
       <div>
-        <mui.AppBar title="Wall Street" showMenuIconButton={false} />
-        <div style={{padding: '1em'}}>
-          <Router.RouteHandler />
-        </div>
+        <mui.AppBar title="Wall Street" onLeftIconButtonTouchTap={this.toggleLeftNav} />
+        <mui.LeftNav
+          ref="leftNav"
+          docked={false}
+          header={header}
+          menuItems={menuItems}
+          onChange={this.onLeftNavChange}
+          selectedIndex={this.getSelectedIndex()}
+        />
+        <Router.RouteHandler />
       </div>
     );
   }
 }
 
 App.childContextTypes = {muiTheme: React.PropTypes.object};
+App.contextTypes = {router: React.PropTypes.func};
 
 export default App;
--- app/routes.jsx
+++ app/routes.jsx
@@ -5,12 +5,26 @@ import Home from './components/home/home';
 import NotFound from './components/not-found/not-found';
 import Location from './components/location/location';
 
+let appRoutesData = [
+  {route: 'location', text: 'Location', handler: Location}
+];
+
 let routes = (
   <Router.Route path="/" handler={App}>
     <Router.DefaultRoute handler={Home}/>
     <Router.NotFoundRoute handler={NotFound} />
-    <Router.Route path="location" handler={Location} />
+    {appRoutesData.map((routeData) => {
+      return (
+        <Router.Route
+          key={routeData.route}
+          path={routeData.route}
+          name={routeData.route}
+          handler={routeData.handler}
+        />
+      );
+    })}
   </Router.Route>
 );
 
 export default routes;
+export {appRoutesData};
