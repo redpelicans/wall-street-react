34e0c24db48266f6e97e430761aac1cc2da560b9
Nicolas Briemant <nicolas.briemant@gmail.com>
2015-08-14 14:14:39 +0000

    add default and notFound routes

--- app/app.jsx
+++ app/app.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-
 import Router from 'react-router';
-let RouteHandler = Router.RouteHandler;
 
 import mui from 'material-ui';
 let ThemeManager = new mui.Styles.ThemeManager();
-
-import Location from './components/location/location';
 
 class App extends React.Component {
   getChildContext() {
@@ -14,24 +10,11 @@ class App extends React.Component {
   }
 
   render() {
-    let gridStyle = {
-      padding: '1em'
-    };
-
     return (
       <div>
         <mui.AppBar title="Wall Street" showMenuIconButton={false} />
-        <RouteHandler />
-        <div className="pure-g" style={gridStyle}>
-          <div className="pure-u-1-3">
-            <Location label="Left"/>
-          </div>
-          <div className="pure-u-1-3">
-            <Location label="Middle"/>
-          </div>
-          <div className="pure-u-1-3">
-            <Location label="Right"/>
-          </div>
+        <div style={{padding: '1em'}}>
+          <Router.RouteHandler />
         </div>
       </div>
     );
--- app/routes.jsx
+++ app/routes.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-
 import Router from 'react-router';
-let Route = Router.Route;
-
 import App from './app';
+import Home from './components/home/home';
+import NotFound from './components/not-found/not-found';
 import Location from './components/location/location';
 
 let routes = (
-  <Route handler={App}>
-    <Route path="location" handler={Location} />
-  </Route>
+  <Router.Route path="/" handler={App}>
+    <Router.DefaultRoute handler={Home}/>
+    <Router.NotFoundRoute handler={NotFound} />
+    <Router.Route path="location" handler={Location} />
+  </Router.Route>
 );
 
 export default routes;
